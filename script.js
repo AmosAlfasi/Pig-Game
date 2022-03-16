1000bff87192f789287893515edcf322a9aeee62
AmosAlfasi <amos13389@gmail.com>
2022-03-16 18:49:07 +0200

    New Game Button is now functioning

--- script.js
+++ script.js
@@ -6,19 +6,35 @@ const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const curr0El = document.getElementById('current--0');
+const curr1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores;
+let currScore;
+let activePlayer;
+let playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  diceEl.classList.add('hidden');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  curr0El.textContent = 0;
+  curr1El.textContent = 0;
+
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+
+  scores = [0, 0];
+  currScore = 0;
+  activePlayer = 0;
+  playing = true;
+};
+init();
 
 const cangePlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -59,7 +75,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2.CHeck if players score is >=100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
       //Finsh game
       document
         .querySelector(`.player--${activePlayer}`)
@@ -76,3 +92,5 @@ btnHold.addEventListener('click', function () {
 
   //Switch to next player
 });
+
+btnNew.addEventListener('click', init);
